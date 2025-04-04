625906df2cf6727bc0d3bce02bcd5eba90b6821e
Les Peabody <lpeabody@gmail.com>
2025-04-04 15:29:30 -0400

    Polymer #59, Issue #17: Refactor to use Polymer template plugin mechanism, add multidev workflows.

---
+++ src/Polymer/Plugin/Template/GitHubWorkflows/PantheonPrMultidevDelete.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DigitalPolygon\PolymerPantheon\Drupal\Polymer\Plugin\Template\GitHubWorkflows;
+
+use DigitalPolygon\Polymer\Robo\Template\GitHub\GitHubWorkflowTemplateBase;
+use DigitalPolygon\Polymer\Robo\Template\Token;
+
+final class PantheonPrMultidevDelete extends GitHubWorkflowTemplateBase {
+
+  public const BASE_FILENAME = 'pantheon-pr-multidev-delete.yml';
+
+  public static function id(): string {
+    return 'github-pantheon-pr-multidev-delete';
+  }
+
+  public function description(): string {
+    return 'A GitHub workflow that runs on pull requests and deletes the associated Pantheon multidev environment.';
+  }
+
+  public function source(): string {
+    return $this
+        ->getConfig()
+        ->get('extension.polymer_pantheon_drupal.root') . '/workflows/github/' . self::BASE_FILENAME;
+  }
+
+  public function destination(): string {
+    return $this->getGitHubWorkflowDir() . '/' . self::BASE_FILENAME;
+  }
+
+  public function tokens(): array {
+    return [
+      new Token('#php-version#', '8.3', true),
+      new Token('#default-branch#', $this->getConfigValue('git.default_branch', 'main'), true),
+      new Token('#pantheon-site-name#', $this->getConfigValue('pantheon.site-info.name'), true),
+    ];
+  }
+
+  public static function collections(): array {
+    $collections = parent::collections();
+    $collections[] = 'pantheon';
+    return $collections;
+  }
+
+}
---
+++ src/Polymer/Plugin/Template/GitHubWorkflows/PantheonPushDev.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DigitalPolygon\PolymerPantheon\Drupal\Polymer\Plugin\Template\GitHubWorkflows;
+
+use DigitalPolygon\Polymer\Robo\Template\GitHub\GitHubWorkflowTemplateBase;
+use DigitalPolygon\Polymer\Robo\Template\Token;
+
+final class PantheonPushDev extends GitHubWorkflowTemplateBase {
+
+  public static function id(): string {
+    return 'github-pantheon-push-dev';
+  }
+
+  public function description(): string {
+    return 'A GitHub workflow that calls the github-pantheon-push workflow to build and push the artifact to the Pantheon dev environment (master branch).';
+  }
+
+  public function source(): string {
+    return $this
+        ->getConfig()
+        ->get('extension.polymer_pantheon_drupal.root') . '/workflows/github/pantheon-push-dev.yml';
+  }
+
+  public function destination(): string {
+    return $this->getGitHubWorkflowDir() . '/pantheon-push-dev.yml';
+  }
+
+  public function tokens(): array {
+    return [
+      new Token('#default-artifact#', $this->getConfigValue('default_artifact', 'main'), true),
+      new Token('#default-branch#', $this->getConfigValue('git.default_branch', 'main'), true),
+    ];
+  }
+
+  public static function collections(): array {
+    $collections = parent::collections();
+    $collections[] = 'pantheon';
+    return $collections;
+  }
+
+}
